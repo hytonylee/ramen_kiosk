16575f4e967c8dd2e884de41f36793089fed94b0
Tony Lee <hy.tony.lee@gmail.com>
2018-03-31 22:33:40 -0700

    organize code

--- ramen_kiosk/app/javascript/components/Header.js
+++ ramen_kiosk/app/javascript/components/Header.js
@@ -5,14 +5,13 @@ import { Media } from 'reactstrap';
 class Header extends React.Component {
   render() {
     return (
-      <header>
-        <div className="top mt-3 d-flex flex-row d-flex justify-content-center">
-          <div><h1>RAMEN </h1></div>
-            <img src="https://goo.gl/5AdCrU" />
-          <div><h1> KIOSK</h1></div>
-        <div className="top-banner"/>
-          hello
-        </div>
+      <header className="mt-3 d-flex flex-column ">
+          <div className="top d-flex justify-content-center d-flex flex-row">
+            <h1>RAMEN </h1><img src="https://goo.gl/5AdCrU" /><h1> KIOSK</h1>
+          </div>
+          <div className="top-banner">
+            hello
+          </div>
       </header>
     )
   }
